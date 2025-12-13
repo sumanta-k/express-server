4680496d16b4592e7adb2f77e09f757090c89d8a
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2025-12-13 21:43:45 +0530

    Revert "Added path /route , Resolves #3"
    This reverts commit af2aaf59c5420e17a52e7f0d9122ffba1e6b03d8.

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ const server = http.createServer((req, res) => {
         res.write("<h1>served from http core module</h1>");
         res.write("<p>This is the website page</p>");
         res.end();
-    } else if (urlPath === "/greet") {
-        res.write("<html>");
-        res.write("<h3>Thank you for coming</h3>");
-        res.write("<strong>Visit Us Again</strong>");
-        res.write("</html>");
-        res.end();
     }
 });
 
